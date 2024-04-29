04dafd9d8fc277d3498cae4c04e80dd0eaaa76f9
Dieny Simon <simon.dieny@gmail.com>
2024-04-29 06:21:59 +0200

    setup --no-routing state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-pokedex-app';
