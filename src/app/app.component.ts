6704f6d13d8a0b819f5ed947331703109bba86b5
Dieny Simon <simon.dieny@gmail.com>
2024-05-15 12:27:08 +0200

    06.11. Correction : Ajouter la taille d’un Pokémon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,17 @@ import { Component, signal } from '@angular/core';
 export class AppComponent {
   name = signal('Pikachu');
   life = signal(21);
+  size = computed(() => {
+    if (this.life() <= 15) {
+      return 'Petit';
+    }
+
+    if (this.life() >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  });
 
   incrementLife() {
     this.life.update((n) => n + 1);
