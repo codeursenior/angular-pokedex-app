e11103156cfdbc538ec339bcf42811e3c8ecf7e0
Dieny Simon <simon.dieny@gmail.com>
2024-05-16 07:02:16 +0200

    07.09. Rétablir les fonctionnalités

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, signal } from '@angular/core';
 import { POKEMON_LIST } from './pokemon-list.fake';
+import { Pokemon } from './pokemon.model';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,23 @@ import { POKEMON_LIST } from './pokemon-list.fake';
 })
 export class AppComponent {
   pokemonList = signal(POKEMON_LIST);
+
+  size(pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+    if (pokemon.life >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  }
+
+  incrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life + 1;
+  }
+
+  decrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life - 1;
+  }
 }
