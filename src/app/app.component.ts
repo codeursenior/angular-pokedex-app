5f070d2e7b873d062070bebe3b0b451a05f4f04d
Dieny Simon <simon.dieny@gmail.com>
2024-05-16 10:50:28 +0200

    09. Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, computed, signal } from '@angular/core';
 import { POKEMON_LIST } from './pokemon-list.fake';
 import { Pokemon } from './pokemon.model';
 import { PokemonBorderDirective } from './pokemon-border.directive';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PokemonBorderDirective],
+  imports: [DatePipe, PokemonBorderDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
