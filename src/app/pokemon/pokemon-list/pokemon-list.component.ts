2ffbb3ee6518e1a56e82b0e5dddadac1528b532a
Dieny Simon <simon.dieny@gmail.com>
2024-05-25 07:09:09 +0200

    step(routes): generate component for pokemon list

---
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-list.component.html',
+  styles: ``
+})
+export class PokemonListComponent {
+
+}
