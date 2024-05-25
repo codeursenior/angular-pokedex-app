7d5dbf9ccfae0637fcd62f01afa062c6d42768a2
Dieny Simon <simon.dieny@gmail.com>
2024-05-25 14:03:05 +0200

    step(routes): add route for pokemon profile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { Routes, provideRouter } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 
 const routes: Routes = [
   { path: 'pokemons', component: PokemonListComponent },
+  { path: 'pokemons/:id', component: PokemonProfileComponent },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
 ];
 
