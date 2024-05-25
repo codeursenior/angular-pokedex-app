c5a9147f8017a558baa350dbb6f3da15b95f59b0
Dieny Simon <simon.dieny@gmail.com>
2024-05-25 13:54:42 +0200

    step(routes): generate component for pokemon profile

---
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-profile.component.html',
+  styles: ``
+})
+export class PokemonProfileComponent {
+
+}
