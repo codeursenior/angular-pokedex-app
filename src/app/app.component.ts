7ce87b5701c94692472e52a2414e3cab83d32886
Dieny Simon <simon.dieny@gmail.com>
2024-05-25 07:15:00 +0200

    step(routes): move code from app component to pokemon list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,10 @@
-import { Component, computed, inject, model, signal } from '@angular/core';
-import { Pokemon } from './pokemon.model';
-import { PokemonBorderDirective } from './pokemon-border.directive';
-import { DatePipe } from '@angular/common';
-import { PokemonService } from './pokemon.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe, PokemonBorderDirective],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  readonly pokemonService = inject(PokemonService);
-  readonly pokemonList = signal(this.pokemonService.getPokemonList());
-  readonly searchTerm = signal('');
-  readonly pokemonListFiltered = computed(() => {
-    return this.pokemonList().filter((pokemon) =>
-      pokemon.name
-        .toLowerCase()
-        .includes(this.searchTerm().trim().toLowerCase())
-    );
-  });
-
-  size(pokemon: Pokemon) {
-    if (pokemon.life <= 15) {
-      return 'Petit';
-    }
-    if (pokemon.life >= 25) {
-      return 'Grand';
-    }
-
-    return 'Moyen';
-  }
-
-  incrementLife(pokemon: Pokemon) {
-    pokemon.life = pokemon.life + 1;
-  }
-
-  decrementLife(pokemon: Pokemon) {
-    pokemon.life = pokemon.life - 1;
-  }
-}
+export class AppComponent {}
--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { PokemonBorderDirective } from '../../pokemon-border.directive';
+import { PokemonService } from '../../pokemon.service';
+import { Pokemon } from '../../pokemon.model';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, PokemonBorderDirective],
   templateUrl: './pokemon-list.component.html',
-  styles: ``
+  styles: ``,
 })
 export class PokemonListComponent {
+  readonly pokemonService = inject(PokemonService);
+  readonly pokemonList = signal(this.pokemonService.getPokemonList());
+  readonly searchTerm = signal('');
+  readonly pokemonListFiltered = computed(() => {
+    return this.pokemonList().filter((pokemon) =>
+      pokemon.name
+        .toLowerCase()
+        .includes(this.searchTerm().trim().toLowerCase())
+    );
+  });
 
+  size(pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+    if (pokemon.life >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  }
+
+  incrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life + 1;
+  }
+
+  decrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life - 1;
+  }
 }
