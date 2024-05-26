cb30eb991cad1af5f2322a3162b18a60265c57cd
Dieny Simon <simon.dieny@gmail.com>
2024-05-26 13:26:41 +0200

    step(routes): setup HTML & Bootstrap for pokemon profile page

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-profile',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, RouterLink],
   templateUrl: './pokemon-profile.component.html',
   styles: ``,
 })
