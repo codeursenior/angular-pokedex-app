64302352d6ad6c47fccf3c4a0b41429938fa105c
Dieny Simon <simon.dieny@gmail.com>
2024-05-26 13:36:31 +0200

    step(routes): add 404 page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { Routes, provideRouter } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'pokemons', component: PokemonListComponent },
   { path: 'pokemons/:id', component: PokemonProfileComponent },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './page-not-found.component.html',
+  styles: ``,
+})
+export class PageNotFoundComponent {}
