f395dd7e6694576c08c98504c61f43a1cd111083
Dieny Simon <simon.dieny@gmail.com>
2024-05-26 07:49:38 +0200

    step(route): get router date in profile component

--- src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
+++ src/app/pokemon/pokemon-profile/pokemon-profile.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-profile',
   standalone: true,
   imports: [],
   templateUrl: './pokemon-profile.component.html',
-  styles: ``
+  styles: ``,
 })
 export class PokemonProfileComponent {
-
+  readonly route = inject(ActivatedRoute);
+  readonly pokemonService = inject(PokemonService);
+  readonly pokemonId = signal(
+    Number(this.route.snapshot.paramMap.get('id'))
+  ).asReadonly();
+  readonly pokemon = signal(
+    this.pokemonService.getPokemonById(this.pokemonId())
+  ).asReadonly();
 }
