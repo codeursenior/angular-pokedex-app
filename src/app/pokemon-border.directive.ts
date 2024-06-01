12d9c303c23d6ecf1ae95fe971c9b861b63b6fb6
Dieny Simon <simon.dieny@gmail.com>
2024-06-01 17:39:49 +0200

    step(forms): refactor to extract pokemon color from directive to pokemon model

--- src/app/pokemon-border.directive.ts
+++ src/app/pokemon-border.directive.ts
@@ -1,11 +1,12 @@
 import { Directive, ElementRef, HostListener, input } from '@angular/core';
+import { getPokemonColor } from './pokemon.model';
 
 @Directive({
   selector: '[appPokemonBorder]',
   standalone: true,
 })
 export class PokemonBorderDirective {
-  pokemonType = input.required();
+  pokemonType = input.required<string>();
   private initialColor: string;
 
   constructor(private el: ElementRef) {
@@ -28,25 +29,6 @@ export class PokemonBorderDirective {
   }
 
   private getBorderColor() {
-    switch (this.pokemonType()) {
-      case 'Feu':
-        return '#EF5350';
-      case 'Eau':
-        return '#42A5F5';
-      case 'Plante':
-        return '#66BB6A';
-      case 'Insecte':
-        return '#8d6e63';
-      case 'Vol':
-        return '#90CAF9';
-      case 'Poison':
-        return '#b388ff';
-      case 'Fée':
-        return '#f8bbd0';
-      case 'Electrik':
-        return '#f4ff81';
-      default:
-        return '#303030';
-    }
+    return getPokemonColor(this.pokemonType());
   }
 }
--- src/app/pokemon.model.ts
+++ src/app/pokemon.model.ts
@@ -1,11 +1,24 @@
-export interface Pokemon {
-  id: number;
-  name: string;
-  picture: string;
-  life: number;
-  damage: number;
-  types: [string, string?, string?];
-  created: Date;
+export function getPokemonColor(type: string) {
+  switch (type) {
+    case 'Feu':
+      return '#EF5350';
+    case 'Eau':
+      return '#42A5F5';
+    case 'Plante':
+      return '#66BB6A';
+    case 'Insecte':
+      return '#8d6e63';
+    case 'Vol':
+      return '#90CAF9';
+    case 'Poison':
+      return '#b388ff';
+    case 'Fée':
+      return '#f8bbd0';
+    case 'Electrik':
+      return '#f4ff81';
+    default:
+      return '#303030';
+  }
 }
 
 export const POKEMON_RULES = {
@@ -16,5 +29,15 @@ export const POKEMON_RULES = {
   MAX_DAMAGE: 10,
   MIN_DAMAGE: 1,
 };
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  picture: string;
+  life: number;
+  damage: number;
+  types: [string, string?, string?];
+  created: Date;
+}
 
 export type PokemonList = Pokemon[];
