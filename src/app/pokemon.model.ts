5d1eb8b0fefd3c05a04b5db0317e112830469ed8
Dieny Simon <simon.dieny@gmail.com>
2024-06-02 07:39:54 +0200

    step(validator): add domain rules

--- src/app/pokemon.model.ts
+++ src/app/pokemon.model.ts
@@ -22,12 +22,17 @@ export function getPokemonColor(type: string) {
 }
 
 export const POKEMON_RULES = {
+  NAME_PATTERN: /^[a-zA-Zéè]+$/,
+  MAX_NAME: 20,
+  MIN_NAME: 3,
   MAX_LIFE: 30,
   HIGH_LIFE: 25,
   LOW_LIFE: 15,
   MIN_LIFE: 10,
   MAX_DAMAGE: 10,
   MIN_DAMAGE: 1,
+  MIN_TYPES: 1,
+  MAX_TYPES: 3,
 };
 
 export interface Pokemon {
