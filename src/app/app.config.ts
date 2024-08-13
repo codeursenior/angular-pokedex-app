d7c724193708fa7142798e0da15a0ae976fe7ea3
Simon Dieny <simon.dieny@non.se.com>
2024-08-13 10:36:10 +0200

    step(http): provide HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.compon
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
+import { provideHttpClient } from '@angular/common/http';
 
 const routes: Routes = [
   { path: 'pokemons', component: PokemonListComponent, title: 'Pokédex' },
@@ -25,5 +26,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 };
